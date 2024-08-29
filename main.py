5c1d8a531d8194d192563bedefd0e29496f0a5e0
Siladittya Manna <sadimanna@users.noreply.github.com>
2024-08-29 09:59:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,26 +5,12 @@ from typing import Sequence, Union
 
 import os
 # os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:1024"
-import barlowtwins
-import byol
-import dcl
-import dclw
-import dino
 import finetune_eval
 import knn_eval
 import linear_eval
-import transfer_tasks
-import simclr, simclrv2
-import directclr
-import simclr_vanilla
-import simclr_encoder
-import swav
-import moco
+import simclrv2
 import torch
 torch.cuda.empty_cache()
-import vicreg
-import tssimclr, tsdcl
-import miov3, miov2, miov1
 from pytorch_lightning import LightningModule, Trainer
 from pytorch_lightning.callbacks import (
     DeviceStatsMonitor,
@@ -41,10 +27,9 @@ from lightly.utils.benchmarking import MetricCallback
 from lightly.utils.dist import print_rank_zero
 
 parser = ArgumentParser("ImageNet ResNet50 Benchmarks")
-parser.add_argument("--train-dir", type=Path, default="C:/Users/ISI_UTS/Siladittya/iclr2023/inexpts/datasets/ImageNet100/train")
-parser.add_argument("--val-dir", type=Path, default="C:/Users/ISI_UTS/Siladittya/iclr2023/inexpts/datasets/ImageNet100/val")
-parser.add_argument("--log-dir", type=Path, default="C:/Users/ISI_UTS/Siladittya/iclr2023/inexpts/benchmark_logs_im100_r50")
-parser.add_argument("--transfer-dir", type=Path, default="/datasets/")
+parser.add_argument("--train-dir", type=Path, default="./datasets/ImageNet100/train")
+parser.add_argument("--val-dir", type=Path, default="./datasets/ImageNet100/val")
+parser.add_argument("--log-dir", type=Path, default="./benchmark_logs_im100")
 parser.add_argument("--batch-size-per-device", type=int, default=256)
 parser.add_argument("--epochs", type=int, default=100)
 parser.add_argument("--num-workers", type=int, default=1)
@@ -58,28 +43,9 @@ parser.add_argument("--num-classes", type=int, default=100)
 parser.add_argument("--skip-knn-eval", action="store_true")
 parser.add_argument("--skip-linear-eval", action="store_true")
 parser.add_argument("--skip-finetune-eval", action="store_false")
-parser.add_argument("--skip-transfer-tasks", action="store_false")
 
 METHODS = {
-    "barlowtwins": {
-        "model": barlowtwins.BarlowTwins,
-        "transform": barlowtwins.transform,
-    },
-    "byol": {"model": byol.BYOL, "transform": byol.transform},
-    "dcl": {"model": dcl.DCL, "transform": dcl.transform},
-    "dclw": {"model": dclw.DCLW, "transform": dclw.transform},
-    "dino": {"model": dino.DINO, "transform": dino.transform},
-    "simclr": {"model": simclr_vanilla.SimCLR, "transform": simclr.transform},
-    "simclrv2": {"model": simclrv2.SimCLR_v2, "transform": simclr.transform},
-    "directclr": {"model": directclr.DirectCLR, "transform": simclr.transform},
-    "swav": {"model": swav.SwAV, "transform": swav.transform},
-    "tssimclr": {"model": tssimclr.TSSimCLR, "transform": simclr.transform},
-    "tsdcl": {"model": tsdcl.TSDCL, "transform": tsdcl.transform},
-    "miov3": {"model": miov3.MIOv3, "transform":miov3.transform},
-    "miov2": {"model": miov2.MIOv2, "transform":miov2.transform},
-    "miov1": {"model": miov1.MIOv1, "transform":miov1.transform},
-    "vicreg": {"model": vicreg.VICReg, "transform": vicreg.transform},
-    "moco": {"model":moco.MoCo, "transform":moco.transform}
+    "simclrv2": {"model": simclrv2.SimCLR_v2, "transform": simclrv2.transform},
 }
 
 
@@ -87,7 +53,6 @@ def main(
     train_dir: Path,
     val_dir: Path,
     log_dir: Path,
-    transfer_dir: Path,
     batch_size_per_device: int,
     epochs: int,
     num_workers: int,
@@ -100,7 +65,6 @@ def main(
     skip_knn_eval: bool,
     skip_linear_eval: bool,
     skip_finetune_eval: bool,
-    skip_transfer_tasks: bool,
     ckpt_path: str = None,
 ) -> None:
     torch.set_float32_matmul_precision("high")
@@ -265,10 +229,9 @@ def pretrain(
         ],
         logger=TensorBoardLogger(save_dir=str(log_dir), name="pretrain_simclr"),
         precision=precision,
-        strategy="auto", #ddp", #_find_unused_parameters_true", #"auto", #ddp", #_find_unused_parameters_true",
+        strategy="auto", #ddp", #_find_unused_parameters_true",
         sync_batchnorm=True,
         # find_unused_parameters = False,
-        # resume_from_checkpoint = ckpt_path,
     )
 
     trainer.fit(
